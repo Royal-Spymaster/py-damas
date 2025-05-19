85ea9f874083afe502248fa8fec8ae251c2f6cc8
Pedro <pedrinho.seco@gmail.com>
2025-05-19 14:48:16 -0300

    reestruturação repo 2

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,93 +1,66 @@
-estado = {
-        
-        'tile_branco':      'Branco',
-        'tile_preto':       'Preto',
-        
-        'peca_branca':      'Peca Branca',
-        'peca_preta':       'Peca Preta',
-        
-        'rainha_branca':    'Rainha Branca',
-        'rainha_preta':     'Rainha Preta',
-        
-        'sem_peca':         'Vazio',
-        }
+#----------Variáveis Globais
+tabuleiro = []
+letras = ['A','B','C','D','E','F','G','H']
+numeros = ['1','2','3','4','5','6','7','8']
 
-#coord = faz_coordenadas()
+#----------Apoio
+def constroi_linha(num,primeiro_tile,peca = 'Vazio',primeira_peca = 0):
+    tiles = ['Quadrado Branco', 'Quadrado Preto']
+    pecas = ['Vazio',peca]
+    linha = []
 
+    for letra in range(8):
+            if letra % 2 == 0:
+                linha.append([letras[letra] + num,tiles[primeiro_tile],pecas[primeira_peca]])
+            else:
+                linha.append([letras[letra] + num,tiles[primeiro_tile-1],pecas[primeira_peca-1]])
+    
+    return linha
+
+#----------Tabuleiro
 def faz_tabuleiro():
-    letras = ['A','B','C','D','E','F','G','H']
-    numeros = ['1','2','3','4','5','6','7','8']
-    
-    lista_local= []
-    tabuleiro = []
-    
 
-    'Estrutura do tabuleiro: tabuleiro com 8 colunas e 8 linhas, totalizando 64 tiles.'
-    'Cada tile tem um valor de coluna, linha e estado atual do tile atribuído. ex. [A1, bV].'
-    
-    for coluna in letras:
-        'tabuleiro gerado baseando se nas linhas e colunas.'
-        'linhas impares tem colunas tem colunas impares brancas. ex. A1 -> Coluna 1, linha 1 -> tile preto.'
-        'linhas pares tem colunas brancas pares. ex. B2 -> Coluna 2, linha 2 -> tile branco.'
-        'linhas <= 3 (index <= 2) inicialmente tem pecas pretas em tiles pretos. ex. A2 -> Coluna 2, linha 1 -> tile preto peca preta.'
-        'linhas >= 6 (index >= 5) inicialmente tem pecas brancas em tiles pretos. ex. F1 -> Coluna 6, linha 1 -> tile preto peca branca.'
-
-        if letras.index(coluna) <= 2:
-            'colunas A, B e C comecam com pecas pretas.'
-            if epar(letras.index(coluna)) == True:
-                for linha in numeros:
-                    if epar(numeros.index(linha)) == True:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_branco'), estado.get('sem_peca')])
-                    else:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_preto'), estado.get('peca_preta')])
+    for num in numeros:
+        if numeros.index(num) < 3:
+            if numeros.index(num) % 2 == 0:
+                tabuleiro.append(constroi_linha(num,0,'Peça Preta',0))
             else:
-                for linha in numeros:
-                    if epar(numeros.index(linha)) == True:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_preto'), estado.get('peca_preta')])
-                    else:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_branco'), estado.get('sem_peca')])
+                tabuleiro.append(constroi_linha(num,1,'Peça Preta',1))
 
-        elif letras.index(coluna) >= 5:
-            'coluna F, G e H comecam com pecas brancas.'
-            if epar(letras.index(coluna)) == True:
-                for linha in numeros:
-                        if epar(numeros.index(linha)) == True:
-                            lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_branco'), estado.get('sem_peca')])
-                        else:
-                            lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_preto'), estado.get('peca_branca')])
+        elif numeros.index(num) >= 5:
+            if numeros.index(num) % 2 == 0:
+                tabuleiro.append(constroi_linha(num,0,'Peça Branca',0))
             else:
-                for linha in numeros:
-                        if epar(numeros.index(linha)) == True:
-                            lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_preto'), estado.get('peca_branca')])
-                        else:
-                            lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_branco'), estado.get('sem_peca')])
+                tabuleiro.append(constroi_linha(num,1,'Peça Branca',1))
+
         else:
-            'tiles sem pecas no comeco'
-            if epar(letras.index(coluna)) == True:
-                for linha in numeros:
-                    if epar(numeros.index(linha)) == True:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_branco'), estado.get('sem_peca')])
-                    else:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_preto'), estado.get('sem_peca')])
+            if numeros.index(num) % 2 == 0:
+                tabuleiro.append(constroi_linha(num,0))
             else:
-                for linha in numeros:
-                    if epar(numeros.index(linha)) == True:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_preto'), estado.get('sem_peca')])
-                    else:
-                        lista_local.append([letras[letras.index(coluna)] + numeros[numeros.index(linha)], estado.get('tile_branco'), estado.get('sem_peca')])
-        tabuleiro.append(lista_local)
-        lista_local = []
+                tabuleiro.append(constroi_linha(num,1))
+                      
+    return tabuleiro
 
-        return tabuleiro
+def imprime_tabuleiro():
+    pecas_imagem = [' ⛀ ',' ⛁ ', ' ⛂ ' , ' ⛃ ','   ']
+    pecas_texto = ['Peça Branca', 'Rainha Branca','Peça Preta','Rainha Preta','Vazio']
+    linhas = []
 
-def epar(num):
-    'Retorna se número é par ou não.'
-    if num % 2 == 0:
-       return True
-    else:
-        return False
+    print('> ')
+    print('>      A    B    C    D    E    F    G    H')
+    print('>     _______________________________________')
 
+    for coluna in range(8):
+        for linha in range(8):
+            for item in pecas_texto:
+                if tabuleiro[coluna][linha][2] == item:
+                    linhas.append(pecas_imagem[pecas_texto.index(item)])
+        linhas_prontas = ' |'.join(linhas)
+        linhas = []
+        print('> ',coluna + 1,'  ','|', linhas_prontas,' |','  ', coluna + 1, sep='')
 
+    print('>     ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾')
+    print('>      A    B    C    D    E    F    G    H')
+    print('> ')
 
-tabuleiro_feito = faz_tabuleiro()
-print(tabuleiro_feito)
+#----------Chamada de Funções
